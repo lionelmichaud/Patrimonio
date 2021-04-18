003f562e061e106840217efd9a7ae5fcd517a2dd
Lionel <lio.michaud@gmail.com>
2021-04-18 15:47:51 +0200

    Initial Commit

---
+++ Shared/PatrimonioApp.swift
@@ -0,0 +1,17 @@
+//
+//  PatrimonioApp.swift
+//  Shared
+//
+//  Created by Lionel MICHAUD on 18/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct PatrimonioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
