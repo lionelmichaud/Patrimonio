c6f80b07baba89ad3588d7c7fe0eb8464ee47bbf
Lionel <lio.michaud@gmail.com>
2022-03-22 22:47:34 +0100

    Codable synthesis for enums with associated values
    SE-0295

--- Shared/Modules/FiscalModel/Sources/FiscalModel/WorkIncome.swift
+++ Shared/Modules/FiscalModel/Sources/FiscalModel/WorkIncome.swift
@@ -11,7 +11,7 @@ import AppFoundation
 
 // MARK: - Revenus du travail
 /// revenus du travail
-public enum WorkIncomeType {
+public enum WorkIncomeType: Codable {
     case salary (brutSalary: Double, taxableSalary: Double, netSalary: Double, fromDate: Date, healthInsurance: Double)
     case turnOver (BNC: Double, incomeLossInsurance: Double)
     
@@ -90,75 +90,6 @@ extension WorkIncomeType: PickableIdentifiableEnumP {
                     - BNC: \(BNC.€String)
                     - Coût annuel assurance perte de revenu: \(incomeLossInsurance.€String)
                     """
-        }
-    }
-}
-
-extension WorkIncomeType: Codable {
-    // coding keys
-    private enum CodingKeys: String, CodingKey {
-        case salary_brutSalary, salary_taxableSalary, salary_netSalary, salary_fromDate, salary_healthInsurance
-        case turnOver_BNC, turnOver_incomeLossInsurance
-    }
-    // error type
-    enum PersonalIncomeTypeCodingError: Error {
-        case decoding(String)
-    }
-    // decode
-    public init(from decoder: Decoder) throws {
-        let values = try decoder.container(keyedBy: CodingKeys.self)
-        // decode .salary
-        if let salary_brutSalary = try? values.decode(Double.self, forKey: .salary_brutSalary) {
-            if let salary_taxableSalary = try? values.decode(Double.self, forKey: .salary_taxableSalary) {
-                if let salary_netSalary = try? values.decode(Double.self, forKey: .salary_netSalary) {
-                    if let salary_fromDate = try? values.decode(Date.self, forKey: .salary_fromDate) {
-                        if let salary_healthInsurance = try? values.decode(Double.self, forKey: .salary_healthInsurance) {
-                            self = .salary(brutSalary      : salary_brutSalary,
-                                           taxableSalary   : salary_taxableSalary,
-                                           netSalary       : salary_netSalary,
-                                           fromDate        : salary_fromDate,
-                                           healthInsurance : salary_healthInsurance)
-                                return
-                            } else {
-                                throw PersonalIncomeTypeCodingError.decoding("Error while decoding '.salary_healthInsurance' ! \(dump("values"))")
-                            }
-                        } else {
-                            throw PersonalIncomeTypeCodingError.decoding("Error while decoding '.salary_fromDate' ! \(dump("values"))")
-                        }
-                } else {
-                    throw PersonalIncomeTypeCodingError.decoding("Error while decoding '.salary_netSalary' ! \(dump("values"))")
-                }
-            } else {
-                throw PersonalIncomeTypeCodingError.decoding("Error while decoding '.salary_taxableSalary' ! \(dump("values"))")
-            }
-        }
-        
-        // decode .turnOver
-        if let value1 = try? values.decode(Double.self, forKey: .turnOver_BNC) {
-            if let value2 = try? values.decode(Double.self, forKey: .turnOver_incomeLossInsurance) {
-                self = .turnOver(BNC: value1, incomeLossInsurance: value2)
-                return
-            } else {
-                throw PersonalIncomeTypeCodingError.decoding("Error while decoding '.turnOver_incomeLossInsurance' ! \(dump("values"))")
-            }
-        }
-        
-        throw PersonalIncomeTypeCodingError.decoding("Whoops! \(dump(values))")
-    }
-    // encode
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        
-        switch self {
-            case .salary (let brutSalary, let taxableSalary, let netSalary, let fromDate, let healthInsurance):
-                try container.encode(brutSalary, forKey: .salary_brutSalary)
-                try container.encode(taxableSalary, forKey: .salary_taxableSalary)
-                try container.encode(netSalary, forKey: .salary_netSalary)
-                try container.encode(fromDate, forKey: .salary_fromDate)
-                try container.encode(healthInsurance, forKey: .salary_healthInsurance)
-            case .turnOver (let BNC, let incomeLossInsurance):
-                try container.encode(BNC, forKey: .turnOver_BNC)
-                try container.encode(incomeLossInsurance, forKey: .turnOver_incomeLossInsurance)
         }
     }
 }
